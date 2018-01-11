b49277e152337bab3773b0b2153c927c0c56d031
geobaldi <geobaldi@gmail.com>
2018-01-11 12:48:31 +0100

    I'm using cmsplugin_filer_image/plugins/image/default.html to render the featured image Some publisher refactoring

--- ripiu/cmsplugin_articles/cms_plugins.py
+++ ripiu/cmsplugin_articles/cms_plugins.py
@@ -7,11 +7,8 @@ from django.conf import settings
 from .models import ArticlePluginModel, SectionPluginModel
 
 
-class ArticlePlugin(CMSPluginBase):
-    model = ArticlePluginModel
-    name = _('Article')
+class HeadedPlugin(CMSPluginBase):
     module = "Ri+"
-    render_template = 'ripiu/cmsplugin_articles/article.html'
     allow_children = True
     fieldsets = (
         (_('Header'), {
@@ -30,7 +27,7 @@ class ArticlePlugin(CMSPluginBase):
     )
 
     def render(self, context, instance, placeholder):
-        context = super(ArticlePlugin, self).render(
+        context = super(HeadedPlugin, self).render(
             context, instance, placeholder
         )
         thumb_opts = {
@@ -55,9 +52,20 @@ class ArticlePlugin(CMSPluginBase):
         context.update({
             'instance': instance,
             'placeholder': placeholder,
-            'thumb_opts': thumb_opts,
+            'featured_image': {
+                'image': instance.featured_image,
+                'thumbnail_option': thumb_opts,
+                'alignment': instance.alignment,
+                # 'alt': instance.featured_image.default_alt_text,
+            }
         })
         return context
+
+
+class ArticlePlugin(HeadedPlugin):
+    model = ArticlePluginModel
+    name = _('Article')
+    render_template = 'ripiu/cmsplugin_articles/article.html'
 
 
 plugin_pool.register_plugin(ArticlePlugin)
@@ -66,54 +74,7 @@ plugin_pool.register_plugin(ArticlePlugin)
 class SectionPlugin(CMSPluginBase):
     model = SectionPluginModel
     name = _('Section')
-    module = "Ri+"
     render_template = 'ripiu/cmsplugin_articles/section.html'
-    allow_children = True
-    fieldsets = (
-        (_('Header'), {
-            'fields': (
-                'title',
-                'subtitle',
-                ('heading_level', 'header_alignment'),
-            )
-        }), (_('Featured image'), {
-            'fields': (
-                'featured_image',
-                'thumbnail_option',
-                'alignment',
-            )
-        }),
-    )
-
-    def render(self, context, instance, placeholder):
-        context = super(SectionPlugin, self).render(
-            context, instance, placeholder
-        )
-        thumb_opts = {
-            'width': settings.RIPIU_ARTICLES_IMAGE_WIDTH,
-            'height': settings.RIPIU_ARTICLES_IMAGE_HEIGHT,
-            'crop': settings.RIPIU_ARTICLES_IMAGE_CROP,
-            'upscale': settings.RIPIU_ARTICLES_IMAGE_UPSCALE,
-        }
-        if instance.alignment:
-            thumb_opts['alignment'] = instance.alignment
-        if instance.thumbnail_option:
-            if instance.thumbnail_option.width:
-                thumb_opts['width'] = instance.thumbnail_option.width
-            if instance.thumbnail_option.height:
-                thumb_opts['height'] = instance.thumbnail_option.height
-            thumb_opts['crop'] = instance.thumbnail_option.crop
-            thumb_opts['upscale'] = instance.thumbnail_option.upscale
-        thumb_opts['size'] = (
-            thumb_opts['width'],
-            thumb_opts['height'],
-        )
-        context.update({
-            'instance': instance,
-            'placeholder': placeholder,
-            'thumb_opts': thumb_opts,
-        })
-        return context
 
 
 plugin_pool.register_plugin(SectionPlugin)
