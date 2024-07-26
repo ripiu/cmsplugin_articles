5093813a83b98a541e6ec19a872fcad37809bd71
geobaldi <matteo@nodi.digital>
2024-07-26 10:15:54 +0200

    stop using ugettext_lazy

--- ripiu/cmsplugin_articles/models.py
+++ ripiu/cmsplugin_articles/models.py
@@ -2,7 +2,7 @@ from cms.models import CMSPlugin
 from cms.models.fields import PageField
 from django.conf import settings
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from djangocms_attributes_field.fields import AttributesField
 
 from .conf import settings as conf  # NOQA
